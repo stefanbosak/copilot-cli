06a186a4378dfa9ebb89dc17326026a853845687
Stefan Bosak <1700523+stefanbosak@users.noreply.github.com>
2026-01-29 23:07:59 +0100

    Aligned Copilot CLI run wrapper
    Signed-off-by: Stefan Bosak <1700523+stefanbosak@users.noreply.github.com>

--- copilot.sh
+++ copilot.sh
@@ -11,24 +11,15 @@ copilot() {
   export DOCKER_GID=$(getent group docker | cut -d: -f3)
 
   docker run -it --rm \
-    # add obtained docker group from host into container
     --group-add "${DOCKER_GID}" \
-    # GitHub Copilot CLI environment variables
     --env-file "${HOME}/.copilot/.env" \
-    # Docker in Docker socket mapping
     -v /var/run/docker.sock:/var/run/docker.sock \
-    # GitHub Copilot CLI configuration storage
     -v "${HOME}/.copilot:/home/node/.copilot" \
-    # workspace used by GitHub Copilot CLI
     -v "${HOME}/workspace:/workspace" \
-    # Docker configuration storage
     -v "${HOME}/.docker:/home/node/.docker:ro" \
-    # Docker MCP toolkit (optional)
     -v "${HOME}/.docker/mcp:/home/node/.docker/mcp" \
-    # set workspace directory
     -w "/workspace" \
     ghcr.io/stefanbosak/copilot-cli:initial \
-    # GitHub Copilot CLI arguments
     copilot --no-auto-update --allow-all-tools "$@"
 }
 
